aafc1227e553bcb104b2a16627af501259db97e7
Erez Sharim <erez@crossid.io>
2024-03-07 16:56:36 +0200

    chore: hide slack endpoints
    hides slack endpoints from openapi schema

--- app/slack/router.py
+++ app/slack/router.py
@@ -10,17 +10,17 @@ router = APIRouter(prefix="/slack", tags=["slack"])
 handler = SlackRequestHandler(app=slack_app)
 
 
-@router.post("/events")
+@router.post("/events", include_in_schema=False)
 async def endpoint(req: Request):
     return await handler.handle(req)
 
 
-@router.get("/install")
+@router.get("/install", include_in_schema=False)
 async def install(req: Request):
     return await handler.handle(req)
 
 
-@router.get("/oauth_redirect")
+@router.get("/oauth_redirect", include_in_schema=False)
 async def oauth_redirect(req: Request):
     return await handler.handle(req)
 
