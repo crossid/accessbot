37c0a896fb4bd23dc0787ef1b2686c4f05fd53ec
Asaf Shakarzy <asaf@crossid.io>
2024-03-13 17:30:51 +0200

    feat: add ENV setting to distinguish between environments

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
-
+    ENV: str = "production"
     DB_URI: str = ""
     VSTORE_URI: str = ""
     VSTORE_EMBEDDING: str = "openai"
