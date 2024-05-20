e7cd6884c35596321377577fc0dc080a54a0fabc
Erez Sharim <erez@crossid.io>
2024-05-20 12:23:48 +0300

    feat: content route filtering
    adds a way to filter content by app_names and directory

--- app/ext_vector_store.py
+++ app/ext_vector_store.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import List, Optional
 
 from sqlalchemy import text
 
@@ -9,13 +9,28 @@ collection_table_name = "langchain_pg_collection"
 embedding_table_name = "langchain_pg_embedding"
 
 
+def prepare_where(directory: Optional[str], app_names: Optional[List[str]]):
+    base_where = "collection_id = :collection_id"
+    if directory is not None:
+        base_where += " and cmetadata::jsonb -> 'directory' = :directory"
+
+    if app_names is not None:
+        apps_str = ", ".join(
+            ["'[\"{name}\"]'::jsonb".format(name=n) for n in app_names]
+        )
+        base_where += f" and cmetadata::jsonb -> 'app' @> ANY(ARRAY[{apps_str}])"
+
+    return base_where
+
+
 def list_documents(
     workspace_id: str,
-    filters: dict[str, Any] = None,
+    app_names: Optional[List[str]] = None,
+    directory: Optional[str] = None,
     offset=0,
     limit=10,
     tx_context: TransactionContext = None,
-) -> tuple[list[Document], int]:
+) -> tuple[List[Document], int]:
     stmt = text(
         f"SELECT * FROM {collection_table_name} WHERE name = :workspace_id limit 1;"
     )
@@ -24,16 +39,17 @@ def list_documents(
     if collection is None:
         return [], 0
 
+    filters = prepare_where(directory=directory, app_names=app_names)
     base_count_query = text(
-        f"select count(*) from {embedding_table_name} where collection_id = :collection_id;"
+        f"select count(*) from {embedding_table_name} where {filters}"
     )
     total_count = tx_context.connection.execute(
         base_count_query,
-        {"collection_id": str(collection.uuid)},
+        {"collection_id": str(collection.uuid), "directory": f'"{directory}"'},
     ).scalar_one()
 
     query = text(
-        f"select * from {embedding_table_name} where collection_id = :collection_id order by uuid limit :limit offset :offset;"
+        f"select * from {embedding_table_name} where {filters} order by uuid limit :limit offset :offset;"
     )
 
     result = tx_context.connection.execute(
@@ -42,6 +58,7 @@ def list_documents(
             "collection_id": str(collection.uuid),
             "limit": limit,
             "offset": offset,
+            "directory": f'"{directory}"',
         },
     )
     docs = []
