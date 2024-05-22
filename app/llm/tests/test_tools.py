6d97c8bbcd87b84f4818e0cb82245d8c7430a841
Erez Sharim <erez@crossid.io>
2024-05-22 15:05:23 +0300

    chore: test webhook impl
    adds test to webhook implementation

--- app/llm/tests/test_tools.py
+++ app/llm/tests/test_tools.py
@@ -1,7 +1,10 @@
 import asyncio
 import unittest
+import unittest.mock
 from typing import Any
+from unittest.mock import patch
 
+from app.llm.tools.provision.webhook import WebhookImpl
 from app.models import User, Workspace
 
 
@@ -54,3 +57,17 @@ class TestCreateTicketTool(unittest.TestCase):
 
         rtt = create_expanded_model(extra_fields=extra_fields)
         print(rtt)
+
+    @patch("requests.post")
+    def test_webhook(self, mock_post):
+        mock_response = unittest.mock.Mock()
+        mock_response.status_code = 200
+        mock_post.return_value = mock_response
+
+        url = "http://example.com/api"
+        expected_body = {"requester_email": "jon.doe@foo.com", "foo": "bar"}
+        whi = WebhookImpl(url=url)
+        ack = asyncio.run(whi.approve_request(**expected_body))
+
+        self.assertTrue(ack)
+        mock_post.assert_called_once_with(url=url, json=expected_body)
