61f446440a12c82d841a5021096e4bbaed7aca6e
Asaf Shakarzy <asaf@crossid.io>
2024-05-27 16:40:22 +0300

    refactor: workspace config ticket_system -> access_requests_method

--- app/llm/tools/consts.py
+++ app/llm/tools/consts.py
@@ -1,5 +1,5 @@
 DATAOWNER_CONFIG_KEY = "data_owner"
-TICKET_SYSTEM_CONFIG_KEY = "ticket_system"
+TICKET_SYSTEM_CONFIG_KEY = "access_requests_method"
 PROVISION_CONFIG_KEY = "provisioning"
 EMAIL_CONFIG_KEY = "email"
 DIRECTORIES_KEY = "directories"
