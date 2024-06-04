49390cf9e790d53de8ce8cdc7c2407a909f20fe9
Asaf Shakarzy <asaf@crossid.io>
2024-06-04 18:32:10 +0300

    fix: required settings should not have defaults
    resolves #116

--- app/settings.py
+++ app/settings.py
@@ -4,19 +4,20 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
     ENV: str = "production"
-    DB_URI: str = ""
-    VSTORE_URI: str = ""
+    DB_URI: str
+    VSTORE_URI: str
     VSTORE_EMBEDDING: str = "openai"
+    # default to empty as in mem vault does not require a value
     VAULT_URI: str = ""
     # oauth
-    OAUTH2_OPENID_CONFIGURATION: str = ""
-    OAUTH2_AUDIENCE: str = ""
+    OAUTH2_OPENID_CONFIGURATION: str
+    OAUTH2_AUDIENCE: str
     # llm
     LLM_MODEL: str = "openai://gpt-4-turbo-preview"
     # messaging
-    SLACK_CLIENT_ID: str = ""
-    SLACK_CLIENT_SECRET: str = ""
-    SLACK_SIGNING_SECRET: str = ""
+    SLACK_CLIENT_ID: str
+    SLACK_CLIENT_SECRET: str
+    SLACK_SIGNING_SECRET: str
 
     USER_STORE_FILE_PATH: str = "tmp/data.json"
 
