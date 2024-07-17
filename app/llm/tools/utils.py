f830e8e6599d36ba46c895492a0d414bcddaca20
Erez Sharim <erez@crossid.io>
2024-07-17 11:55:43 +0300

    feat: data owner conversation language
    data owner conversation should be in the same language of the previous conversation.
    data owner message should be better formatted.

--- app/llm/tools/utils.py
+++ app/llm/tools/utils.py
@@ -3,6 +3,7 @@ from typing import Any, Optional
 from langchain.tools import Tool
 from pydantic.v1 import BaseModel, Field, create_model
 
+from app.i18n import i18n
 from app.models import Conversation, Workspace
 from app.models_stores import ConversationStore
 from app.services import (
@@ -66,3 +67,34 @@ def update_conv(
         updates=updates,
         tx_context=tx_context,
     )
+
+
+class MsgContent:
+    full: str
+    approval_q: str
+
+    def __init__(self, full: str, approval_q: str):
+        self.full = full
+        self.approval_q = approval_q
+
+
+def get_do_msg_content(
+    lang: str, requester: str, conv_summary: str, app_name: str, **kwargs
+) -> MsgContent:
+    i = i18n(lang=lang)
+    approval_q = i.t("approval_q").capitalize()
+    kwargs_str = "\n".join(f"{i.t(key)}: {value}" for key, value in kwargs.items())
+    full = f"""
+    {i.t("hello")}, {i.t("request_waiting")}: {requester}.\n
+    *{i.t("basic_info")}*:\n
+    - {i.t("app_name").capitalize()}: {app_name}\n
+    - {i.t("summary").capitalize()}: {conv_summary}\n
+    \n
+    *{i.t("my_recommendation").capitalize()}*:
+    {kwargs_str}
+    \n
+    {approval_q}
+    """
+
+    msg = MsgContent(full=full, approval_q=approval_q)
+    return msg
