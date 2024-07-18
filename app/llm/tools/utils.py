ace5e1c535c0ec9701c5ebb2255afbf32c540ef6
Asaf Shakarzy <asaf@crossid.io>
2024-07-18 21:56:10 +0300

    fix: data owner markdown message formatting

--- app/llm/tools/utils.py
+++ app/llm/tools/utils.py
@@ -1,8 +1,5 @@
 from typing import Any, Optional
 
-from langchain.tools import Tool
-from pydantic.v1 import BaseModel, Field, create_model
-
 from app.i18n import i18n
 from app.models import Conversation, Workspace
 from app.models_stores import ConversationStore
@@ -10,6 +7,8 @@ from app.services import (
     factory_app_store,
 )
 from app.sql import SQLAlchemyTransactionContext
+from langchain.tools import Tool
+from pydantic.v1 import BaseModel, Field, create_model
 
 
 def get_tools_for_workspace_and_conversation(
@@ -83,18 +82,18 @@ def get_do_msg_content(
 ) -> MsgContent:
     i = i18n(lang=lang)
     approval_q = i.t("approval_q").capitalize()
-    kwargs_str = "\n".join(f"{i.t(key)}: {value}" for key, value in kwargs.items())
+    kwargs_str = "\n".join(f"- {i.t(key)}: {value}" for key, value in kwargs.items())
     full = f"""
-    {i.t("hello")}, {i.t("request_waiting")}: {requester}.\n
-    *{i.t("basic_info")}*:\n
-    - {i.t("app_name").capitalize()}: {app_name}\n
-    - {i.t("summary").capitalize()}: {conv_summary}\n
-    \n
-    *{i.t("my_recommendation").capitalize()}*:
-    {kwargs_str}
-    \n
-    {approval_q}
-    """
+{i.t("hello")}, {i.t("request_waiting")}: {requester}.\n\n
+**{i.t("basic_info")}**:
+- {i.t("app_name").capitalize()}: {app_name}
+- {i.t("summary").capitalize()}: {conv_summary}\n
+\n
+**{i.t("my_recommendation").capitalize()}**:\n
+{kwargs_str}
+\n
+{approval_q}
+"""
 
     msg = MsgContent(full=full, approval_q=approval_q)
     return msg
