10724e91488740f106b2ec60290033a5aae0aa03
Erez Sharim <erez@crossid.io>
2024-09-11 18:35:28 +0300

    adds user access fetching to user data interface

--- app/llm/tools/user_data/iface.py
+++ app/llm/tools/user_data/iface.py
@@ -4,5 +4,11 @@ from typing import Any
 
 class UserDataInterface(ABC):
     @abstractmethod
-    async def get_user_data(self, user_email, **kwargs) -> dict[str, Any]:
+    async def get_user_data(self, user_email: str, **kwargs) -> dict[str, Any]:
+        pass
+
+    @abstractmethod
+    async def get_user_access(
+        self, user_email: str, app_names: list[str] = [], **kwargs
+    ) -> dict[str, Any]:
         pass
