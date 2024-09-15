ff30f8db6b5be2d83808c685e55bf4ecf0953d04
Erez Sharim <erez@crossid.io>
2024-09-15 16:42:57 +0300

    changes content upload to accept base metadata

--- app/data_fetching/utils.py
+++ app/data_fetching/utils.py
@@ -9,10 +9,11 @@ from app.id import generate
 class Doc(BaseModel):
     id: Optional[str] = None
     display_name: Optional[str] = None
-    apps: Optional[list[str]]
-    directory: Optional[str]
+    apps: Optional[list[str]] = None
+    directory: Optional[str] = None
     content: Optional[str]
-    external_id: Optional[str]
+    external_id: Optional[str] = None
+    base_metadata: Optional[dict] = {}
 
 
 def prepare_metadata_ids_content(docs: List[Doc]):
@@ -21,12 +22,14 @@ def prepare_metadata_ids_content(docs: List[Doc]):
     content = []
 
     for doc in docs:
-        dmeta = {
+        dmeta = doc.base_metadata | {
             "directory": doc.directory,
             "app": doc.apps,
             "created_at": datetime.now().isoformat(),
             "display_name": doc.display_name or "",
         }
+        # filter None attributes
+        dmeta = {k: v for k, v in dmeta.items() if v is not None}
         metadatas.append(dmeta)
         ids.append(doc.external_id if doc.external_id is not None else generate())
         content.append(doc.content)
